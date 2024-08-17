dac96fa96f691bc42a5756eac83706222c36a19d
wanchanok062 <anvacom2@gmail.com>
2024-08-17 14:44:56 +0700

    [update] settings and config database

--- src/config.ts
+++ src/config.ts
@@ -12,3 +12,8 @@ dotenv.config({ path: envFilePath });
 
 // Export the PORT variable
 export const PORT = process.env.PORT ?? 3000;
+export const DatabaseHost = process.env.DB_HOST;
+export const DatabaseName = process.env.DB_NAME;
+export const DatabaseUser = process.env.DB_USER;
+export const DatabasePassword = process.env.DB_PASSWORD;
+export const DB_DIALECT = process.env.DB_DIALECT;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
 import express, { Request, Response } from 'express';
 import { PORT } from './config';
+import { sequelize } from './config/database';
 const app = express();
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript with Express!');
 });
+
+async function initialize() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+        await sequelize.sync();
+        console.log('Database synced');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+initialize();
 
 app.listen(PORT, () => {
     console.log(
