6c6ac481fec6feaadb21c0d7d16d871107a31f11
wanchanok062 <anvacom2@gmail.com>
2024-10-20 19:44:58 +0700

    [update] add router

--- src/api/router/index.ts
+++ src/api/router/index.ts
@@ -2,13 +2,17 @@ import { Router } from 'express';
 import adminRouter from '../admin/admin-router';
 import customerRouter from '../customer/customer-router';
 import authRouter from '../auth-google/auth-google-router';
-import CodeDiscountCategoryRouter from '../code-discount-category/code-discount-category-router';
+import codeDiscountCategoryRouter from '../code-discount-category/code-discount-category-router';
+import codeDiscountTypeRouter from '../code-discount-type/code-discount-type-router';
+import codeDiscount from '../code-discount/code-discount-router';
 
 const router = Router();
 
 router.use('/api/v1/admin', adminRouter);
 router.use('/api/v1/customer', customerRouter);
 router.use('/auth', authRouter);
-router.use('/api/v1/code-discount-category', CodeDiscountCategoryRouter);
+router.use('/api/v1/code-discount-category', codeDiscountCategoryRouter);
+router.use('/api/v1/code-discount-type', codeDiscountTypeRouter);
+router.use('/api/v1/code-discount', codeDiscount);
 
 export default router;
